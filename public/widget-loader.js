cc8eb0eaaca93fde703717241fb55f2e8c59bcf7
simmmel <106965495+simmmel@users.noreply.github.com>
2024-08-03 20:35:59 +0900

    add widget url determined during build process

--- public/widget-loader.js
+++ public/widget-loader.js
@@ -1,19 +1,15 @@
-(function () {
-  var iframe = document.createElement("iframe");
-  iframe.style.position = "fixed";
-  iframe.style.bottom = "20px";
-  iframe.style.right = "20px";
-  iframe.style.width = "300px";
-  iframe.style.height = "400px";
-  iframe.style.border = "none";
-  iframe.style.zIndex = "9999";
+(function() {
+    var iframe = document.createElement('iframe');
+    iframe.style.position = 'fixed';
+    iframe.style.bottom = '20px';
+    iframe.style.right = '20px';
+    iframe.style.width = '300px';
+    iframe.style.height = '400px';
+    iframe.style.border = 'none';
+    iframe.style.zIndex = '9999';
 
-  // ToDo: set to localhost or deployed URL, depending on the environment
-  iframe.src = "https://your-nextjs-app.com/widget";
+    // This placeholder will be replaced at build time
+    iframe.src = '%%WIDGET_URL%%';
 
-  document.body.appendChild(iframe);
-
-  window.addEventListener("message", function (event) {
-    if (event.origin !== "https://your-nextjs-app.com") return;
-  });
+    document.body.appendChild(iframe);
 })();
