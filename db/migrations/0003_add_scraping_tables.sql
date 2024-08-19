b03179b52848ef1853c7564555822ea4d94afb22
simmmel <106965495+simmmel@users.noreply.github.com>
2024-08-19 16:50:19 +0900

    db migration fix

--- db/migrations/0003_add_scraping_tables.sql
+++ db/migrations/0003_add_scraping_tables.sql
@@ -1,3 +1,9 @@
+CREATE TABLE scraping_run (
+  id SERIAL PRIMARY KEY,
+  data_source_id INT NOT NULL REFERENCES data_source(id),
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TYPE scraping_url_status AS ENUM ('QUEUED', 'PROCESSING', 'CANCELLED', 'COMPLETED', 'FAILED');
 
 CREATE TABLE scraping_url (
@@ -7,10 +13,4 @@ CREATE TABLE scraping_url (
   status scraping_url_status NOT NULL,
   created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE scraping_run (
-  id SERIAL PRIMARY KEY,
-  data_source_id INT NOT NULL REFERENCES data_source(id),
-  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
