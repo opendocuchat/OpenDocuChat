69927ebd259469937db24544059a285b6837717f
simmmel <106965495+simmmel@users.noreply.github.com>
2024-08-26 14:28:21 +0900

    db migration fix

--- db/migrations/0005_add_status_to_scraping_run.sql
+++ db/migrations/0005_add_status_to_scraping_run.sql
@@ -1,3 +1,7 @@
 ALTER TYPE scraping_url_status RENAME TO scraping_status;
 
-ALTER TABLE scraping_run ADD COLUMN status scraping_status NOT NULL;
+ALTER TABLE scraping_run ADD COLUMN status scraping_status;
+
+UPDATE scraping_run SET status = 'COMPLETED';
+
+ALTER TABLE scraping_run ALTER COLUMN status SET NOT NULL;
