80777658b9cd11cb82c88d70bfd587ed7f9820b7
simmmel <106965495+simmmel@users.noreply.github.com>
2024-08-27 15:51:03 +0900

    using exclusion constraint instead

--- db/migrations/0006_add_isindexed_to_scraping_url.sql
+++ db/migrations/0006_add_isindexed_to_scraping_url.sql
@@ -1,5 +1,8 @@
-ALTER TABLE scraping_url ADD COLUMN is_indexed BOOLEAN NOT NULL;
-
-ALTER TABLE scraping_url ADD CONSTRAINT unique_indexed_url UNIQUE (url) WHERE is_indexed = TRUE;
+ALTER TABLE scraping_url ADD COLUMN is_indexed BOOLEAN;
 
 UPDATE scraping_url SET is_indexed = FALSE;
+
+ALTER TABLE scraping_url ALTER COLUMN is_indexed SET NOT NULL;
+
+-- Exclusion constraint to ensure uniqueness of indexed URLs
+ALTER TABLE scraping_url ADD CONSTRAINT unique_indexed_url EXCLUDE USING btree (url WITH =) WHERE (is_indexed = TRUE);
